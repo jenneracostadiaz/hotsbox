781d05a85dbf12a728eebe9b5e009b7ec5d757e6
Jenner Acosta Diaz <my@jenner.pe>
2023-11-01 13:22:53 -0500

    Update Migrations for Hosting

--- database/migrations/2023_11_01_181027_create_hostings_table.php
+++ database/migrations/2023_11_01_181027_create_hostings_table.php
@@ -13,7 +13,37 @@ return new class extends Migration
     {
         Schema::create('hostings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('orders_id');
+            $table->unsignedBigInteger('products_id');
+            $table->unsignedBigInteger('servers_id');
+            $table->date('regdate')->nullable();
+            $table->unsignedBigInteger('domain_id')->nullable();
+            $table->text('paymentmethod')->nullable();
+            $table->integer('qty')->nullable();
+            $table->decimal('firstpaymentamount', 16, 2)->nullable();
+            $table->decimal('amount', 16, 2)->nullable();
+            $table->text('billingcycle')->nullable();
+            $table->date('nextduedate')->nullable();
+            $table->date('nextinvoicedate')->nullable();
+            $table->date('termination_date')->nullable();
+            $table->date('completed_date')->nullable();
+            $table->enum('domainstatus', ['Pending', 'Active', 'Suspended', 'Terminated', 'Cancelled', 'Fraud', 'Completed'])->nullable();
+            $table->text('username')->nullable();
+            $table->text('password')->nullable();
+            $table->text('notes')->nullable();
+            $table->integer('diskusage')->nullable();
+            $table->integer('disklimit')->nullable();
+            $table->integer('bwusage')->nullable();
+            $table->integer('bwlimit')->nullable();
+            $table->dateTime('lastupdate')->nullable();
             $table->timestamps();
+
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('NO ACTION');
+            $table->foreign('orders_id')->references('id')->on('orders')->onDelete('NO ACTION');
+            $table->foreign('products_id')->references('id')->on('products')->onDelete('CASCADE');
+            $table->foreign('servers_id')->references('id')->on('servers')->onDelete('NO ACTION');
+            $table->foreign('domain_id')->references('id')->on('domains')->onDelete('NO ACTION');
         });
     }
 
